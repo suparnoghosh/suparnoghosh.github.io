fea11bd102a936ad92c20ca43970ba80dc50e5e5
suparnoghosh <suparnog2005@gmail.com>
2026-03-13 15:17:14 +0000

    Deploying to gh-pages from @ suparnoghosh/suparnoghosh.github.io@047c06f4b638367b597d6134b85c6d18c9282b6c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -42,11 +42,6 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
             },},{id: "projects-calculus-of-variations-notes",
           title: 'Calculus of Variations Notes',
           description: "Notes from my internship at TIFR-CAM",
