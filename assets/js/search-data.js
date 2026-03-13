74346fb4b1fb3dd84c57f92f9c744cbb2ca330d7
suparnoghosh <suparnog2005@gmail.com>
2026-03-13 14:57:20 +0000

    Deploying to gh-pages from @ suparnoghosh/suparnoghosh.github.io@28a581f9d023cdeb6a4185e5006dfac3ada20744 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,19 +16,19 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/books/";
           },
-        },{id: "nav-projects",
-          title: "Projects",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
         },{id: "nav-blog",
           title: "Blog",
           description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/blog/";
+          },
+        },{id: "nav-projects",
+          title: "Projects",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/projects/";
           },
         },{id: "nav-cv",
           title: "cv",
