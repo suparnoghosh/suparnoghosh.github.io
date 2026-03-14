eef38c23657f01c967f5a0d2f877124bdd6f475a
suparnoghosh <suparnog2005@gmail.com>
2026-03-14 16:03:02 +0000

    Deploying to gh-pages from @ suparnoghosh/suparnoghosh.github.io@58c8b3ffd054b226f0c79af31d32f3878bc8d628 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -52,6 +52,11 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/chhobirdeshe/";
+            },},{id: "books-harry-potter-and-the-cursed-child",
+          title: 'Harry Potter and the Cursed Child',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/cursedchild/";
             },},{id: "books-the-da-vinci-code",
           title: 'The Da Vinci Code',
           description: "",
